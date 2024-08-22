4a10799db7009af0da7eeae305a016afd6078d92
ben270724 <ngocben2707@gmail.com>
2024-08-22 08:56:12 +0700

    up doc

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,9 @@ use App\Models\Product;
 use App\Mail\PostPublished;
 use App\DataTables\UsersDataTable;
 use App\Models\Feature;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::get('/', function () {
     $feature = Feature::all(); // Fetch all features from the database
     return view('ashop', ['features' => $feature]); // Pass features to the view
 });
-
 Route::get('login', [App\Http\Controllers\LoginController::class, 'handleLogin'])->name('login');
 Route::get('register', [App\Http\Controllers\LoginController::class, 'register'])->name('register');
